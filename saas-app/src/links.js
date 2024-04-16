a19df22e1b7764af2fa0fdb8ed6a7543bacef0c5
saiteja-adapa-2909 <98104890+saiteja-adapa-2909@users.noreply.github.com>
2024-04-16 23:26:17 +0530

    Update links.js

--- saas-app/src/links.js
+++ saas-app/src/links.js
@@ -1,4 +1,4 @@
 // const baseURL = "https://wk4h5tms-4000.inc1.devtunnels.ms";
-const baseURL = "http://localhost:4000";
-
+// const baseURL = "http://localhost:4000";
+const baseURL = "https://marriage-management-admin-se.vercel.app/";
 export default baseURL;
